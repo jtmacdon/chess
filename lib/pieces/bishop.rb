43c9cbb7fcaf66fde82c1aa589285b40aafd9d7c
Josh <jtmacdon@live.com>
2020-08-05 15:42:13 -0300

    add moves using permuation to create the array

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -6,6 +6,9 @@ class Bishop < Piece
   def initialize(color, position)
     super(color, position)
     @icon = (@color == "white" ? "\u2657" : "\u265D").encode('utf-8')
-    @moves
+    #creates array with all possible +/- permutations for each number
+    @moves = []
+    [[1, -1], [2, -2], [3, -3], [4, -4], [5, -5], [6, -6], [7, -7]].each { |x|
+     x.repeated_permutation(2).to_a.each { |y| @moves << y} }
   end
 end
